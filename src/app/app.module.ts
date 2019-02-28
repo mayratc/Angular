9fac468171bdd43ae764f4d5ea67c818dd824e28
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-27 23:50:21 -0500

    se importo el componente al archivo app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { videoJuegosComponent } from './videojuegos/videojuegos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    videoJuegosComponent
   ],
   imports: [
     BrowserModule,
