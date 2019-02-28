b22fd32a5373648f5797971e83ef3e9e5a6cb0a2
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-28 00:47:47 -0500

    plantilla en linea de un fichero html de templateUrl

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -2,18 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'videojuegos',
-  template:`
-    <h2>Componente de {{name}}</h2>
-    <h3 [style.background]='color'
-        *ngIf="mostrar_retro != true">EL MEJOR JUEGO ES: {{mejor_juego}}</h3>
-    <h3 [style.background]='color'
-        *ngIf="mostrar_retro ">EL MEJOR JUEGO ES: {{mejor_juego_retro}}</h3>
-
-    <h2>Listado de videojuegos</h2>
-    <ul>
-        <li *ngFor="let game of videojuegos">{{game}}</li>
-    </ul>
-  `
+  templateUrl:'./videojuegos.component.html'
 })
 
 export class videoJuegosComponent {
