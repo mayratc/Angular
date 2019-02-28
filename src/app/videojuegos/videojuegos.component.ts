ae3a350851e87a8db89d7d12042dd55db2897835
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-28 00:26:10 -0500

    se creo una directiva condicional '*ngIf'

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -4,11 +4,17 @@ import { Component } from '@angular/core';
   selector:'videojuegos',
   template:`
     <h2>Componente de {{name}}</h2>
-    <h3>EL MEJOR JUEGO ES: {{mejor_juego}}</h3>
+    <h3 [style.background]='color'
+        *ngIf="mostrar_retro != true">EL MEJOR JUEGO ES: {{mejor_juego}}</h3>
+    <h3 [style.background]='color'
+        *ngIf="mostrar_retro ">EL MEJOR JUEGO ES: {{mejor_juego_retro}}</h3>
   `
 })
 
 export class videoJuegosComponent {
     public name = 'videojuegos 2018';
-    public mejor_juego = 'GTA 5'
+    public mejor_juego = 'GTA 5';
+    public mejor_juego_retro = 'Super Mario 64';
+    public mostrar_retro = true;
+    public color = this.mostrar_retro ? 'yellow':'red';
 }
