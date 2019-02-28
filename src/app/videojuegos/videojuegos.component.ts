295482b7339cec742bf42c813e4c5d5aaed0e1fc
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-27 23:59:53 -0500

    se creo una propiedad del componente con la variable mejor_juego

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:'videojugos',
+  selector:'videojuegos',
   template:`
     <h2>Componente de {{name}}</h2>
+    <h3>EL MEJOR JUEGO ES: {{mejor_juego}}</h3>
   `
 })
 
 export class videoJuegosComponent {
     public name = 'videojuegos 2018';
+    public mejor_juego = 'GTA 5'
 }
