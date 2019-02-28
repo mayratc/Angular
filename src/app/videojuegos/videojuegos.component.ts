f9401139e6c1f5ec2263c4a917de30924939b5a3
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-28 00:35:05 -0500

    se creo un '*ngFor', el cual itera el array videojuego

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
         *ngIf="mostrar_retro != true">EL MEJOR JUEGO ES: {{mejor_juego}}</h3>
     <h3 [style.background]='color'
         *ngIf="mostrar_retro ">EL MEJOR JUEGO ES: {{mejor_juego_retro}}</h3>
+
+    <h2>Listado de videojuegos</h2>
+    <ul>
+        <li *ngFor="let game of videojuegos">{{game}}</li>
+    </ul>
   `
 })
 
@@ -17,4 +22,11 @@ export class videoJuegosComponent {
     public mejor_juego_retro = 'Super Mario 64';
     public mostrar_retro = true;
     public color = this.mostrar_retro ? 'yellow':'red';
+    public videojuegos = [
+        'Los simpson hit and run',
+        'Assins creed',
+        'Fornite',
+        'Candy Crush',
+        'Tekken'
+    ]
 }
